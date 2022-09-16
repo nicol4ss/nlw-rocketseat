a2637af5d0bcd4bd3e597044c3ddc38834ac84f4
Nicolas Elias <nicolaselias@pm.me>
2022-09-15 23:35:38 -0300

    Aula 2 front done

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,9 +5,13 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
-        'nlw-gradient': "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 44.57%);",
+        'nlw-gradient': "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 44.57%)",
+        'game-gradient': "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
         galaxy: "url('/background-galaxy.png')"
       },
     },
